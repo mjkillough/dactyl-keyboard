36b4df1f60f21242e6507c00874968d3c22265a1
Michael Killough <michaeljkillough@gmail.com>
2020-11-23 16:00:09 +0000

    Add a rough draft of a bottom case.
    Based on danielfullmer:adereth-new-bottom.

--- src/dactyl_keyboard/util.clj
+++ src/dactyl_keyboard/util.clj
@@ -7,10 +7,10 @@
          (map (partial apply hull)
               (partition 3 1 shapes))))
 
-(defn bottom [height p]
+(defn bottom [height & p]
   (->> (project p)
        (extrude-linear {:height height :twist 0 :convexity 0})
        (translate [0 0 (/ height 2)])))
 
-(defn bottom-hull [p]
+(defn bottom-hull [& p]
   (hull p (bottom 1 p)))
